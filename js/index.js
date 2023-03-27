ecbe4cd0b77f5ef06433c770c2e2ae41f43895fd
M-Rani <student.ajc@Gmail.com>
2023-03-26 20:21:09 -0700

    ...

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 function insertValue(num) {
   document.form.text.value+=num;
 }
+
+function equalstoo() {
+  var equation = document.form.text.value;
+  if(equation) {
+    document.form.text.value = eval(document.form.text.value);
+  }
+}
+
+function clean() {
+  document.form.text.value = "";
+}
