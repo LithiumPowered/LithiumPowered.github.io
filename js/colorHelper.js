607cc91afe26081256b0e8a0a073e3e063c97147
M-Rani <student.ajc@Gmail.com>
2023-06-02 20:29:16 -0700

    ...

--- js/colorHelper.js
+++ js/colorHelper.js
@@ -5,8 +5,8 @@ const mainBody = document.getElementById("mainBody")
 const imageFiles = ["img/image1.webp", "img/image2.webp", "img/image3.webp"];
 
 const colors = [
-  { color: "#ffffff", background: "#1D1D20", hlcol: "#dfdfdf", linkcol: "#a2a2a2", hovercol: "#ffffff" }, // colors for image 1
-  { color: "#B7B7B9", background: "#222735", hlcol: "#C4C1BD", linkcol: "#9A9BA3", hovercol: "#999FB3" }, // colors for image 2
+  { color: "#ffffff", background: "#1D1D20", hlcol: "#dfdfdf", linkcol: "#a2a2a2", hovercol: "#FFFFFF" }, // colors for image 1
+  { color: "#B7B7B9", background: "#222735", hlcol: "#C89C5D", linkcol: "#9A9BA3", hovercol: "#FFFFFF" }, // colors for image 2
   { color: "#36691F", background: "#313630", hlcol: "#B77281", linkcol: "#85889F", hovercol: "#C89C5D" }, // colors for image 3
 ];
 
